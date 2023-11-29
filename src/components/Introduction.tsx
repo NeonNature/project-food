6eae071ebca3bbd8de0d66ff21e36e947f4bd61d
NeonNature <mustachemaung@gmail.com>
2023-11-29 13:24:43 +0630

    chore: add intro images and update rough layout

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,42 @@
+import '../styles/intro.scss'
+
 const Introduction = () => {
   return (
     <>
-      <div />
+      <div className="section-intro">
+        <div className="row">
+          <div className="col">
+            <div>ABOUT DELIVER X</div>
+            <div>
+              We are here to help amazing restaurants get great customers
+            </div>
+            <div>
+              At morbi adipiscing sit sed consectetur senectus Dignissim sed
+              amet tincidunt vitae ultricies ultrices amet fermentum amet neque
+              et id sed lacinia massa pretium aliquam libero et.
+            </div>
+          </div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div>10M+</div>
+            <div>Happy customers</div>
+          </div>
+          <div className="col">
+            <div>500k+</div>
+            <div>Restaurants available</div>
+          </div>
+          <div className="col">
+            <div>30M+</div>
+            <div>Successful deliveries</div>
+          </div>
+          <div className="col">
+            <div>99.9%</div>
+            <div>Customer satisfaction</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
